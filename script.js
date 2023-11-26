ac833fe0f8bd69fd4fb93cc4f80b9ff623afb99d
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-26 18:57:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const inputField = document.getElementById('fname');
+
+  // Function to convert input to uppercase
+  function convertToUppercase() {
+    const value = inputField.value;
+    inputField.value = value.toUpperCase();
+  }
+
+  // Event listener for input field losing focus
+  inputField.addEventListener('blur', convertToUppercase);
+});
